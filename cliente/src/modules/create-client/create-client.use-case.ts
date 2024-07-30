79fb286479a72ffd86a11b4dd7fad820a9abedfd
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-30 17:13:49 -0300

    FIX: Bug da migração corrigido

--- cliente/src/modules/create-client/create-client.use-case.ts
+++ cliente/src/modules/create-client/create-client.use-case.ts
@@ -1,31 +1,39 @@
-import { prismaClient } from "../../infra/database/prismaClient"
+import { prismaClient } from "../../infra/database/prismaClient";
 
 type CreateClientRequest = {
-    name: string
-    password: string
-    email: string
-    phone: number
-
-}
+  name: string;
+  password: string;
+  email: string;
+  phone: string; // Alterado para string
+};
 
 export class CreateClientUseCase {
-    constructor() {}
+  constructor() {}
 
-    async execute(data: CreateClientRequest) {
-        const customer = await prismaClient.client.findFirst({
-            where: {
-                email: data.email,
-            },
-        })
-        if (customer) throw new Error('Customer já existe');
-
-        const customerCreated = await prismaClient.client.create({
-            data: {
-                ...data,
-            },
-        })
-
-        return customerCreated
-        
+  async execute(data: CreateClientRequest) {
+    // Verificação para garantir que todos os campos obrigatórios estão presentes
+    if (!data.name || !data.password || !data.email || !data.phone) {
+      throw new Error("Todos os campos são obrigatórios");
     }
- }
+
+    // Verificação para garantir que o cliente não existe
+    const customer = await prismaClient.client.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (customer) {
+      throw new Error('Customer já existe');
+    }
+
+    // Criação do cliente
+    const customerCreated = await prismaClient.client.create({
+      data: {
+        ...data,
+      },
+    });
+
+    return customerCreated;
+  }
+}
