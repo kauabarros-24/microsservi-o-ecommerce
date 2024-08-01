b8edea3ae29752f4c972b3cc0307c84df624f2c8
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-08-01 15:04:45 -0300

    Reiniciando

--- pedidos/src/server.ts
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-
-const port = process.env.PORT ?? 3002;
-app.use(express.json())
-
-app.listen(port, () => console.log(`Server client running in port ${port}`))
--- produto/src/server.ts
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express()
-
-const port = process.env.PORT ?? 3003
-
-app.listen(port, () => console.log(`Running produtos in port ${port}`))
